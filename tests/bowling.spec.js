41e4874879e489ea48ef05d057f7c3e757f801b8
JSlovak <jasmine.slovak@gmail.com>
2017-03-31 09:22:17 -1000

    Initial Commit

---
+++ tests/bowling.spec.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6*/
+
+var chai = require("chai");
+var expect = chai.expect;
+var bowling = require("../bowling");
+
+describe("add", function(){
+
+  // it("should be a function", function(){
+  //   expect(add).to.be.a("function");
+  // });
+
+
+});
