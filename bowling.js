d79b6cd47c58d7574a05f0f005381a174ba31cfa
JSlovak <jasmine.slovak@gmail.com>
2017-03-31 16:49:04 -1000

    Created 5 tests, 3 currently passed. Working on spare and strike test cases.

--- bowling.js
+++ bowling.js
@@ -1,6 +1,27 @@
 
 
-module.exports = function() {
+module.exports = function(frames) {
 
+  var score = 0;
 
+  for (var i= 0; i < frames.length; i++){
+
+    var frameScore = frames[i].ball1 + frames[i].ball2;
+
+    if (!(frames[i].hasOwnProperty("ball1")) || !(frames[i].hasOwnProperty("ball2")) || !(frames[i].hasOwnProperty("strike"))|| !(frames[i].hasOwnProperty("spare"))){
+      throw new Error ("We are not able to get your score. Maybe get a beer? ");
+    }
+
+    if ( frame[i].strike === true){
+      score += frameScore;
+
+    }
+    if (frame[i].spare === false){
+
+    }
+
+    score += frameScore;
+  }
+
+  return score;
 };
--- tests/bowling.spec.js
+++ tests/bowling.spec.js
@@ -4,11 +4,138 @@ var chai = require("chai");
 var expect = chai.expect;
 var bowling = require("../bowling");
 
+var oneFrame = [ { ball1: 2,
+                    ball2: 3,
+                    spare: false,
+                    strike: false
+                  }
+                ];
+
+var twoFrames =  [ { ball1: 2,
+                      ball2: 3,
+                      spare: false,
+                      strike: false
+                    },
+                    { ball1: 2,
+                      ball2: 3,
+                      spare: false,
+                      strike: false
+                    }
+                  ];
+
+var goodBowl = [  { ball1: 10,
+                    ball2: 0,
+                    spare: false,
+                    strike: true
+                  },
+                  { ball1: 7,
+                    ball2: 3,
+                    spare: true,
+                    strike: false
+                  },
+                  { ball1: 5,
+                    ball2: 5,
+                    spare: true,
+                    strike: true
+                  },
+                  { ball1: 0,
+                    ball2: 10,
+                    spare: true,
+                    strike: false
+                  },
+                  { ball1: 10,
+                    ball2: 0,
+                    spare: false,
+                    strike: true
+                  }
+              ];
+
+var strikeBowl = [  { ball1: 10,
+                    ball2: 0,
+                    spare: false,
+                    strike: true
+                  },
+                  { ball1: 10,
+                    ball2: 0,
+                    spare: false,
+                    strike: true
+                  },
+                  { ball1: 10,
+                    ball2: 0,
+                    spare: false,
+                    strike: true
+                  },
+                  { ball1: 10,
+                    ball2: 0,
+                    spare: false,
+                    strike: true
+                  }
+              ];
+
+var spareBowl = [  { ball1: 3,
+                    ball2: 7,
+                    spare: true,
+                    strike: false
+                  },
+                  { ball1: 9,
+                    ball2: 1,
+                    spare: true,
+                    strike: false
+                  },
+                  { ball1: 5,
+                    ball2: 5,
+                    spare: true,
+                    strike: false
+                  },
+                  { ball1: 2,
+                    ball2: 8,
+                    spare: true,
+                    strike: false
+                  },
+                  { ball1: 2,
+                    ball2: 3,
+                    spare: false,
+                    strike: false
+                  },
+                   { ball1: 0,
+                    ball2: 5,
+                    spare: false,
+                    strike: false
+                  }
+              ];
+
+
+
 describe("add", function(){
 
-  // it("should be a function", function(){
-  //   expect(add).to.be.a("function");
-  // });
+  it("should be a function", function(){
+    expect(bowling).to.be.a("function");
+  });
+
+  it("should return a final-score in a number data-type", function(){
+    expect(bowling(oneFrame)).to.be.a("number");
+    expect(bowling.bind(null,[{}])).to.throw(Error);
+  });
+
+  it("should take the argument 'frames' values of keys 'frameScore' and add it to final-score ", function(){
+    expect(bowling(oneFrame)).to.equal(5);
+    expect(bowling(twoFrames)).to.equal(10);
+  });
+
+  it("should be able to determine if a frame was a spare and award bonus points", function(){
+    expect(bowling(goodBowl)).to.equal(75);
+    expect(bowling(spareBowl)).to.equal(68);
+
+  });
+
+  it("should be able to determine if a frame was a strike and award bonus points", function(){
+    expect(bowling(goodBowl)).to.equal(75);
+    expect(bowling(strikeBowl)).to.equal(70);
+    expect(bowling(spareBowl)).to.equal(68);
+
+  });
+
+
 
 
 });
